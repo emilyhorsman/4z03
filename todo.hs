7b03479efc91185d63d62fd77eb9ed780e3860e8
Emily Horsman <me@emilyhorsman.com>
2017-03-28 16:52:30 -0400

    Add stdin handler

--- todo.hs
+++ todo.hs
@@ -1,4 +1,6 @@
 import           Control.Applicative
+import           Control.Concurrent
+import           Control.Monad
 import           Data.Time.Clock
 import           Data.Time.Format
 
@@ -10,3 +12,13 @@ parseDate = parseTimeM True defaultTimeLocale
 parseDateHeading :: String -> Maybe UTCTime
 parseDateHeading input =
     parseDate "=== %b %a %d ===" input <|> parseDate "=== %a %d ===" input
+
+
+handleInput :: String -> IO ()
+handleInput l =
+    threadDelay (truncate 3e6) >> putStrLn l
+
+
+main :: IO ()
+main =
+    forever $ getLine >>= forkIO . handleInput
