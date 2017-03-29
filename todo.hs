f5a7714c886baf747f3f27b7a82b576c8b03a618
Emily Horsman <me@emilyhorsman.com>
2017-03-29 16:03:10 -0400

    Let's do it over HTTP!

--- todo.hs
+++ todo.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
 import           Control.Applicative
 import           Control.Concurrent
 import           Control.Concurrent.STM
 import           Control.Monad
+import           Data.ByteString.Builder
+import qualified Data.ByteString.Lazy     as LazyBS
 import           Data.Time.Clock
 import           Data.Time.Format
+import           Network.HTTP.Types
+import           Network.Wai
+import           Network.Wai.Handler.Warp (run)
 import           Text.Printf
 
 
@@ -16,7 +22,7 @@ parseDateHeading input =
     parseDate "=== %b %a %d ===" input <|> parseDate "=== %a %d ===" input
 
 
-handleInput :: (Num a, PrintfArg a) => TVar a -> String -> IO ()
+handleInput :: TVar Int -> String -> IO ()
 handleInput n l = do
     threadDelay (truncate 1e6)
     putStrLn l
@@ -24,12 +30,29 @@ handleInput n l = do
     putStrLn $ printf "Incremented %d times!" count
 
 
-increment :: Num a => TVar a -> STM a
+increment :: TVar Int -> STM Int
 increment num =
     readTVar num >>= writeTVar num . (+1) >> readTVar num
 
 
+intToLBS :: Int -> LazyBS.ByteString
+intToLBS = toLazyByteString . intDec
+
+
+cmdEcho :: IO ()
+cmdEcho = do
+    n <- atomically $ newTVar (0 :: Int)
+    forever $ getLine >>= forkIO . handleInput n
+
+
+app :: TVar Int -> Application
+app counter _ respond =
+    threadDelay (truncate 1e6)
+    >> (atomically $ increment counter)
+    >>= (\n -> respond $ responseLBS status200 [] (intToLBS n))
+
+
 main :: IO ()
 main = do
-    n <- atomically $ newTVar (0 :: Integer)
-    forever $ getLine >>= forkIO . handleInput n
+    n <- atomically $ newTVar (0 :: Int)
+    run 8080 (app n)
